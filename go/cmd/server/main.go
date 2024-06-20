aa47dc1f4a4be9ce70e77cf1681e3ca5e2ae423e
Willy Angga <ampedwilly@gmail.com>
2024-06-20 14:52:33 +0700

    Add grpc reflection dependencies for evans cli

--- go/cmd/server/main.go
+++ go/cmd/server/main.go
@@ -9,6 +9,7 @@ import (
 	"github.com/willyamped/grpc/pb"
 	"github.com/willyamped/grpc/service"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
 func main() {
@@ -22,6 +23,7 @@ func main() {
 	laptopServer := service.NewLaptopServer(laptopStore, imageStore, ratingStore)
 	grpcServer := grpc.NewServer()
 	pb.RegisterLaptopServiceServer(grpcServer, laptopServer)
+	reflection.Register(grpcServer)
 
 	address := fmt.Sprintf("0.0.0.0:%d", *port)
 	listener, err := net.Listen("tcp", address)
